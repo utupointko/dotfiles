31b903d8ef499242ee9f1fa3d315ca690e4933f8
Arslan Abdizhalilov <arslan.abdizhalilov@gmail.com>
2024-05-19 21:44:33 +0200

    update wezterm and onboarding script
    1. add key bindings to wezterm
    2. add raycast installation to onboarding script
    3. add error handling and comments to onboarding script

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- this is where you actually apply your config choices
-
 config.initial_cols = 160
 config.initial_rows = 48
 
@@ -17,6 +16,33 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 12
+
+-- keybindings
+config.keys = {
+    -- reload configuration
+    {key="r", mods="SUPER|SHIFT", action=wezterm.action.ReloadConfiguration},
+
+    -- close pane
+    {key="w", mods="SUPER|SHIFT", action=wezterm.action.CloseCurrentPane{confirm=true}},
+
+    -- split horizontally
+    {key="d", mods="SUPER|SHIFT", action=wezterm.action.SplitHorizontal{domain="CurrentPaneDomain"}},
+
+    -- split vertically
+    {key="v", mods="SUPER|SHIFT", action=wezterm.action.SplitVertical{domain="CurrentPaneDomain"}},
+
+    -- resize panes
+    {key="LeftArrow", mods="SUPER|SHIFT", action=wezterm.action.AdjustPaneSize{"Left", 5}},
+    {key="RightArrow", mods="SUPER|SHIFT", action=wezterm.action.AdjustPaneSize{"Right", 5}},
+    {key="UpArrow", mods="SUPER|SHIFT", action=wezterm.action.AdjustPaneSize{"Up", 5}},
+    {key="DownArrow", mods="SUPER|SHIFT", action=wezterm.action.AdjustPaneSize{"Down", 5}},
+
+    -- change focus
+    {key="LeftArrow", mods="SUPER", action=wezterm.action.ActivatePaneDirection "Left"},
+    {key="RightArrow", mods="SUPER", action=wezterm.action.ActivatePaneDirection "Right"},
+    {key="UpArrow", mods="SUPER", action=wezterm.action.ActivatePaneDirection "Up"},
+    {key="DownArrow", mods="SUPER", action=wezterm.action.ActivatePaneDirection "Down"},
+}
 
 -- and finally, return the configuration to wezterm
 return config
--- onboard.sh
+++ onboard.sh
@@ -1,52 +1,100 @@
 #!/bin/sh
 
-function install_raycast() {
-    brew uninstall --cask raycast
-    # config load
+# exit script immediately on error
+set -e
 
-    echo "> installed raycast"
+function install_raycast() {
+    echo "> installing raycast..."
+
+    brew install -q --cask raycast || {
+        echo "error: failed to install raycast"
+        exit 1
+    }
 }
 
 function install_dependencies() {
-    brew install --cask wezterm
-    brew tap homebrew/cask-fonts
-    brew install homebrew/cask/font-meslo-lg-nerd-font
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    brew install zsh-autosuggestions
-    brew install zsh-syntax-highlighting
-    brew install fzf
-    brew install fd
-    git clone https://github.com/junegunn/fzf-git.sh.git ~/.fzf-git.sh
-    brew install git-delta
-    brew install bat
-    brew install eza
-    brew install zoxide
-    brew install tldr
-    brew install thefuck
+    echo "> installing dependencies..."
+    
+    # install fonts
+    brew tap -q homebrew/cask-fonts && brew install -q --cask font-meslo-lg-nerd-font || {
+        echo "error: failed to install fonts"
+        exit 1
+    }
 
-
-    echo "> installed dependencies"
+    # install wezterm
+    brew install -q --cask wezterm || {
+        echo "error: failed to install wezterm"
+        exit 1
+    }
+    
+    # install oh my zsh
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || {
+            echo "error: failed to install oh my zsh"
+            exit 1
+        }
+    fi
+    
+    # install zsh plugins
+    brew install -q zsh-autosuggestions zsh-syntax-highlighting || {
+        echo "error: failed to install zsh plugins"
+        exit 1
+    }
+    
+    # install additional tools
+    brew install -q fzf fd git-delta bat eza zoxide tldr thefuck || {
+        echo "error: failed to install additional tools"
+        exit 1
+    }
+    rm -rf ~/.fzf-git.sh && git clone -q https://github.com/junegunn/fzf-git.sh.git ~/.fzf-git.sh || {
+        echo "error: failed to clone junegunn/fzf-git.sh.git repository"
+        exit 1
+    }
 }
 
 function copy_dotfiles() {
-    cp .wezterm.lua ~/
-    cp .zshrc ~/
-    
-    echo "> copied dotfiles"
+    echo "> copying dotfiles..."
+
+    # coping wezterm files
+    cp .wezterm.lua ~/.wezterm.lua || {
+        echo "error: failed to copy .wezterm.lua"
+        exit 1
+    }
+
+    # coping zsh files
+    cp .zshrc ~/.zshrc || {
+        echo "error: failed to copy .zshrc"
+        exit 1
+    }
 }
 
 function set_gitconfig() {
-    cat .gitconfig >> ~/.gitconfig
+    echo "> setting gitconfig..."
 
-    echo "> set gitconfig"
+    cat .gitconfig >> ~/.gitconfig || {
+        echo "error: failed to set gitconfig"
+        exit 1
+    }
 }
 
 function reload_configs() {
-    zsh -c 'source ~/.zshrc && echo "> reloaded configs"'
+    echo "> reloading configs..."
+
+    zsh -c 'source ~/.zshrc' || {
+        echo "error: failed to reload configs"
+        exit 1
+    }
 }
 
-# install_raycast
-# install_dependencies
-# set_gitconfig
-copy_dotfiles
-reload_configs
+function start_setup() {
+    # uncomment the functions you want to run
+    # install_raycast
+    install_dependencies
+    copy_dotfiles
+    set_gitconfig
+    reload_configs
+}
+
+start_setup
+
+echo "> done ✔️"
